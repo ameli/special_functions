7eedf2a55d44a240e4e7e9bdf9504d0838ed5bac
Siavash Ameli <sia.sunrise@gmail.com>
2022-03-30 21:42:39 -0700

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../'))
 
 # The following is treatment exclusively for CYTHON. Since I build cython
