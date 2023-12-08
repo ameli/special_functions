01e678060012acf2d9c614c3484479dfeb958f9c
Siavash Ameli <sia.sunrise@gmail.com>
2023-12-08 12:54:40 -0800

    test 22

--- tools/cibw_before_build_macos-2.sh
+++ tools/cibw_before_build_macos-2.sh
@@ -4,10 +4,6 @@ set -xe
 # curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/11-arm-alpha2/gfortran-ARM-11.0-BigSur.pkg -o gfortran.pkg
 curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-monterey/gfortran-Intel-12.1-Monterey.dmg -o gfortran.dmg
 
-pwd
-ls
-
-
 # GFORTRAN_SHA256=$(shasum -a 256 gfortran.dmg)
 # KNOWN_SHA256="e2e32f491303a00092921baebac7ffb7ae98de4ca82ebbe9e6a866dd8501acdf  gfortran.dmg"
 #
@@ -20,7 +16,8 @@ hdiutil attach -mountpoint /Volumes/gfortran gfortran.dmg
 
 ls /Volumes/gfortran
 
-sudo installer -pkg /Volumes/gfortran/gfortran.pkg -target /
+# sudo installer -pkg /Volumes/gfortran/gfortran.pkg -target /
+sudo installer -pkg /Volumes/gfortran/*/gfortran.pkg -target /
 # sudo installer -pkg gfortran.pkg -target /
 type -p gfortran
 which gfortran
