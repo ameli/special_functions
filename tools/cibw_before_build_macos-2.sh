d6cb723cee0fdc5d24eefc261ece5c24a4141866
Siavash Ameli <sia.sunrise@gmail.com>
2023-12-08 12:32:12 -0800

    test 18

--- tools/cibw_before_build_macos-2.sh
+++ tools/cibw_before_build_macos-2.sh
@@ -1,6 +1,10 @@
 set -xe
 
-curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-monterey/gfortran-ARM-12.1-Monterey.dmg -o gfortran.dmg
+# curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-monterey/gfortran-ARM-12.1-Monterey.dmg -o gfortran.dmg
+curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/11-arm-alpha2/gfortran-ARM-11.0-BigSur.pkg -o gfortran.pkg
+
+pwd
+ls
 
 # GFORTRAN_SHA256=$(shasum -a 256 gfortran.dmg)
 # KNOWN_SHA256="e2e32f491303a00092921baebac7ffb7ae98de4ca82ebbe9e6a866dd8501acdf  gfortran.dmg"
@@ -10,8 +14,12 @@ curl -L https://github.com/fxcoudert/gfortran-for-macOS/releases/download/12.1-m
 #   exit 1
 # fi
 
-hdiutil attach -mountpoint /Volumes/gfortran gfortran.dmg
-sudo installer -pkg /Volumes/gfortran/gfortran.pkg -target /
+# hdiutil attach -mountpoint /Volumes/gfortran gfortran.dmg
+# sudo installer -pkg /Volumes/gfortran/gfortran.pkg -target /
+sudo installer -pkg gfortran.pkg -target /
 type -p gfortran
 which gfortran
 echo $MACOSX_DEPLOYMENT_TARGET
+sw_vers
+uname -av
+uname -m
