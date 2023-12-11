1e9fcde509afef878ac48834139bb8ee692a71f5
Siavash Ameli <sia.sunrise@gmail.com>
2023-12-11 13:59:04 -0800

    cond up 2

--- .cirrus.star
+++ .cirrus.star
@@ -1,13 +1,7 @@
-# The guide to programming cirrus-ci tasks using starlark is found at
-# https://cirrus-ci.org/guide/programming-tasks/
-#
-# In this simple starlark script we simply check conditions for whether
-# a CI run should go ahead. If the conditions are met, then we just
-# return the yaml containing the tasks to be run.
-
 load("cirrus", "env", "fs", "http")
 
 def main(ctx):
 
-    return fs.read("tools/ci/cirrus_deploy_pypi.yml")
-    # return fs.read("tools/ci/cirrus_deploy_conda.yml")
+    # return fs.read("tools/ci/cirrus_deploy_pypi.yml")
+    # return fs.read("tools/ci/cirrus_deploy_conda_linux_aarchi64.yml")
+    return fs.read("tools/ci/cirrus_deploy_conda_macosx_arm64.yml")
--- tools/wheels/upload_wheels.sh
+++ tools/wheels/upload_wheels.sh
@@ -1,54 +1,122 @@
-set_travis_vars() {
-    # Set env vars
-    echo "TRAVIS_EVENT_TYPE is $TRAVIS_EVENT_TYPE"
-    echo "TRAVIS_TAG is $TRAVIS_TAG"
-    if [[ "$TRAVIS_EVENT_TYPE" == "push" && "$TRAVIS_TAG" == v* ]]; then
-      IS_PUSH="true"
+set -xe
+
+
+# ================
+# install anaconda
+# ================
+
+install_anaconda() {
+
+    # Parameters:
+    # $1 is OS, and it can be "linux_aarch64" or "macosx_arm64
+    #
+    # Usage:
+    # install_anaconda "linux_aarch64"
+    # install_anaconda "macosx_arm64"
+
+    MINICONDA_URL="https://repo.anaconda.com/miniconda/"
+    if [ "$1"=="linux_aarch64" ];
+    then
+        URL=${MINICONDA_URL}"Miniconda3-latest-Linux-aarch64.sh"
+    elif [ "$1"=="macosx_arm64" ];
+    then
+        URL=${MINICONDA_URL}"Miniconda3-latest-MacOSX-arm64.sh"
     else
-      IS_PUSH="false"
+        echo "OS or platform is invalid."
+        return 1;
     fi
-    if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
-      IS_SCHEDULE_DISPATCH="true"
+
+    # install miniconda in the parent directory of the current directory,
+    # because the source code of the package is in the current directory, and
+    # if we also install miniconda3 in the same directory of the package source
+    # cod, when we use conda-build, it gives error as it also tries to build
+    # miniconda as well as the code.
+    CURRENT_DIR=$PWD
+    PARENT_DIR=$(dirname $CURRENT_DIR)
+
+    # install miniconda for uploading to anaconda
+    wget -q ${URL} -O ${PARENT_DIR}miniconda.sh
+    bash ${PARENT_DIR}/miniconda.sh -b -p ${PARENT_DIR}/miniconda3
+    ${PARENT_DIR}/miniconda3/bin/conda init bash
+    source ${PARENT_DIR}/miniconda3/bin/activate
+}
+
+
+# =====================
+# build upload anaconda
+# =====================
+
+build_upload_anaconda() {
+
+    # Parameters:
+    # $1 is OS, and it can be "linux_aarch64" or "macosx_arm64
+    #
+    # Usage:
+    # build_upload_anaconda "linux_aarch64"
+    # build_upload_anaconda "macosx_arm64"
+
+    # Conda executable
+    export PATH=$(conda info --root):$PATH
+    export PATH=$(conda info --root)/bin:$PATH
+
+    conda install -y anaconda-client conda-build
+
+    # Anaconda account
+    ANACONDA_USERNAME="s-ameli"
+
+    # Upload sdist
+    if compgen -G "./dist/*.gz"; then
+        anaconda -t ${ANACONDA_API_TOKEN} upload --force \
+            -u ${ANACONDA_USERNAME} ./dist/*.gz
     else
-      IS_SCHEDULE_DISPATCH="false"
+        echo "Dist files do not exist"
+    fi
+
+    # Determine the sub-directory where the conda builds the package depending
+    # on the operating system and platform
+    if [ "$1"=="linux_aarch64" ];
+    then
+        BUILD_SUBDIR="linux-aarch64"
+    elif [ "$1"=="macosx_arm64" ];
+        BUILD_SUBDIR="osx-arm64"
+    else
+        echo "OS or platform is invalid."
+        return 1;
+    fi
+
+    # Build wheels and upload them automatically
+    if compgen -G "./dist/*.whl"; then
+        conda-build --output-folder conda-bld .
+        anaconda -t ${ANACONDA_API_TOKEN} upload --force \
+            -u ${ANACONDA_USERNAME} conda-bld/${BUILD_SUBDIR}/*.tar.bz2
+    else
+        echo "Wheel files do not exist"
+        return 1
     fi
 }
-set_upload_vars() {
-    echo "IS_PUSH is $IS_PUSH"
-    echo "IS_SCHEDULE_DISPATCH is $IS_SCHEDULE_DISPATCH"
-    if [[ "$IS_PUSH" == "true" ]]; then
-        echo push and tag event
-        export ANACONDA_ORG="multibuild-wheels-staging"
-        export TOKEN="$NUMPY_STAGING_UPLOAD_TOKEN"
-        export ANACONDA_UPLOAD="true"
-    elif [[ "$IS_SCHEDULE_DISPATCH" == "true" ]]; then
-        echo scheduled or dispatched event
-        export ANACONDA_ORG="scientific-python-nightly-wheels"
-        export TOKEN="$NUMPY_NIGHTLY_UPLOAD_TOKEN"
-        export ANACONDA_UPLOAD="true"
+
+
+# ==================
+# upload wheels pypi
+# ==================
+
+upload_wheels_pypi() {
+
+    # Install pip and twine
+    python -m pip install --upgrade pip
+    python -m pip install twine
+
+    PYPI_USERNAME="__token__"
+
+    if [[ -z ${PYPI_PASSWORD} ]]; then
+        echo no pypi password set, not uploading
     else
-        echo non-dispatch event
-        export ANACONDA_UPLOAD="false"
-    fi
-}
-upload_wheels() {
-    echo ${PWD}
-    if [[ ${ANACONDA_UPLOAD} == true ]]; then
-        if [[ -z ${TOKEN} ]]; then
-            echo no token set, not uploading
+        if compgen -G "./dist/*.whl"; then
+            echo "Found wheel"
+            twine upload ./dist/* -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
         else
-            # sdists are located under dist folder when built through setup.py
-            if compgen -G "./dist/*.gz"; then
-                echo "Found sdist"
-                anaconda -q -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./dist/*.gz
-            elif compgen -G "./wheelhouse/*.whl"; then
-                echo "Found wheel"
-                anaconda -q -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./wheelhouse/*.whl
-            else
-                echo "Files do not exist"
-                return 1
-            fi
-            echo "PyPI-style index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"
+            echo "Wheel files do not exist"
+            return 1
         fi
     fi
 }
